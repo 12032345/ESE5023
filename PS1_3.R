78f04ea2dde6fb39dda0849cd2d91355b0eaf035
12032345 <70840449+12032345@users.noreply.github.com>
2020-10-09 22:13:36 +0800

    Update PS1_3.R
    A more concise solution

--- PS1_3.R
+++ PS1_3.R
@@ -1,19 +1,8 @@
-Pascal_triangle <- function(x){
-  M <- matrix(nrow=x,ncol=x)
-  for(i in 1:x){
-    M[i,1] <- 1
-    M[i,i] <- 1
-  }
-  for(a in 3:x){
-    for(b in 2:x){
-      M[a,b] <- M[a-1,b-1]+M[a-1,b]
-    }
-  }
-  for(i in 1:x){
-    M[i,1] <- 1
-    M[i,i] <- 1
-  }
-  print(M)
+Pascal <- function(x) {  #帕斯卡三角
+  lapply(0:x, function(i) choose(i, 0:i)) #参考https://blog.csdn.net/the_sad_of_night/article/details/82803882?utm_medium=distribute.pc_relevant.none-task-blog-BlogCommendFromMachineLearnPai2-2.channel_param&depth_1-utm_source=distribute.pc_relevant.none-task-blog-BlogCommendFromMachineLearnPai2-2.channel_param
 }
-Pascal_triangle(4)
+Pascal_triangle <- function(x){  #打印第x行
+  print(Pascal(x)[x])
+}
+Pascal_triangle(100)
 
