f54578d3b19b8e484886abf23cef8d316f146a97
12032345 <70840449+12032345@users.noreply.github.com>
2020-10-11 12:34:22 +0800

    Add files via upload

---
+++ PS1_7.R
@@ -0,0 +1,13 @@
+data <- read.csv('2005西双版纳降雨量.csv')
+
+data2  <- data %>%
+  group_by(月)%>%
+  summarize(month_max = max(总降水量.mm.))
+
+time <- data2$月
+max_jsl <- data2$month_max
+plot(time,max_jsl, type="l", col="blue")
+
+data %>%
+  group_by(月)%>%
+  summarize(month_max = max(总降水量.mm.),month_min = min(总降水量.mm.),month_mean = mean(总降水量.mm.),month_median = median(总降水量.mm.),month_var = var(总降水量.mm.))
