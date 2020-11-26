7459e25a58b95d37e67f1d30f569810fdb821f07
MingYang-Hydrate <71815635+MingYang-Hydrate@users.noreply.github.com>
2020-11-26 16:23:20 +0800

    Update PS3_5.R
    good work

--- PS3_5.R
+++ PS3_5.R
@@ -11,3 +11,9 @@ abline(fit, lwd = 3, col = "#ee3f4d")
 
 #5.4
 summary(fit)$coefficients
+
+# MingYANG noticed：
+# you sould make the intercept to be zero and recount the slop
+# you should make full use of these data, not just to make a translation 
+# and the revised slop should be 0.001921806
+# the end
