897de8b75afb477ebe1a878bfd03b7dde41715e7
MingYang-Hydrate <71815635+MingYang-Hydrate@users.noreply.github.com>
2020-11-26 16:12:45 +0800

    Update PS3_3.R
    right conclusion but wrong analysis

--- PS3_3.R
+++ PS3_3.R
@@ -9,3 +9,23 @@ Nonpregnant_vegetarians<-c(210,139,172,198,177,NA,NA,NA,NA,NA,NA,NA)
 # do a one-way anova to prove that pregnant vegetarians tend to have lower zinc levels than pregnant nonvegetarians
 zinc <- aov(Pregnant_nonvegetarians ~ Pregnant_vegetarians)
 summary(zinc)
+
+# MingYANG noticed :
+# data “NA” can be counted into the anova in your method
+# and you did`t make a plot about this problem
+# so try the following code:
+preveg<-"initiate"
+preveg[1:6]<-"pregnveg"
+preveg[7:18]<-"pregvege"
+preveg[19:23]<-"uprgveg"
+
+zinc <- c(185,189,187,181,150,176,171,174,202,171,207,125,189,179,163,174,184,186,210,139,172,198,177)
+prevegdata<-cbind(preveg,zinc)
+data_tbl <- as_tibble(prevegdata)
+
+datatbl <- data_tbl %>% 
+  filter(preveg != "uprgveg")
+
+anova_one_way <- aov(zinc ~ preveg, data = datatbl)
+summary(anova_one_way)
+# the end
